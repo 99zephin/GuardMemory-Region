c02c8f22b1bf772ba3a938c4ba5d91be4b1d24ed
zephin <32177494+99zephin@users.noreply.github.com>
2022-12-23 16:16:45 +0000

    Create memory.cpp

---
+++ memory.cpp
@@ -0,0 +1,47 @@
+#include <Windows.h>
+
+void gaurd_memory() {
+	unsigned char* memory_region = NULL;
+	LPVOID memory_address = NULL;
+
+	//Define the memory address of where you are executing your file
+
+	//Allocate a memory region using PAGE_NOACCESS, which gaurds the memory region
+	memory_region = (unsigned char*)VirtualAlloc(memory_address, sizeof(memory_address),
+		MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE | PAGE_NOACCESS);
+
+	//Check if allocation was succesfull
+	if (memory_region == NULL) {
+		printf(_("Invalid memory region \n"));
+
+		//Here you can try allocate memory at an arbitrary position
+		memory_region = (unsigned char*)VirtualAlloc(memory_address, sizeof(memory_address),
+			MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE | PAGE_NOACCESS);
+
+		//If it fails again, that means you are out of memory
+		if (memory_region == NULL) {
+			SetLastError(ERROR_OUTOFMEMORY);
+		}
+	}
+
+	//Check if the gaurded memory region has been accessed
+	//To be more efficient, you could run this in a thread, and close the thread after you have free'd all memory
+	MEMORY_BASIC_INFORMATION mbi{};
+	VirtualQuery(memory_region, &mbi, sizeof(mbi));
+	if (mbi.Protect != PAGE_NOACCESS) {
+		//Clear and free all memory of your PE
+		ZeroMemory(memory_address, sizeof(memory_address));
+		VirtualFree(memory_address, sizeof(memory_address), MEM_RELEASE);
+
+		exit(0);
+	}
+
+	//Here you want to map whatever you need
+	//I would also recommened converting it into a byte array (can use HxD)
+
+	//Erase all the memory from that specific address
+	ZeroMemory(memory_address, sizeof(memory_address));
+
+	//Free the memory
+	VirtualFree(memory_address, sizeof(memory_address), MEM_RELEASE);
+}
